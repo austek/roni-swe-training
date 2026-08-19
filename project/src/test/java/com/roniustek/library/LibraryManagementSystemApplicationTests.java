1c1aa45a534060a878ea4a0dcbccfddfe83397f8
austek <13117393+austek@users.noreply.github.com>
2026-08-19 16:02:08 +0100

    feat: bootstrap Spring Boot skeleton for library-management-system

---
+++ project/src/test/java/com/roniustek/library/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,11 @@
+package com.roniustek.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
